68ee4034c4566182197176ae46dfbfcf30c27f5d
ViniciusRCampos <vinicius_uo@hotmail.com>
2023-07-01 19:14:20 -0300

    feat: OffersController and getOffers function

---
+++ app/Http/Controllers/OffersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Services\OffersService;
+
+class OffersController extends Controller
+{
+    public function getOffers(Request $request){
+        
+        $offersService  = new OffersService();
+        $cpf = $request->cpf;
+        $data = $offersService->getOffers($cpf);
+
+        if(!$data){
+            return response()->json("Não existem ofertas para esse CPF, verifique os dados e tente novamente", 422);
+        } 
+            return response()->json($data, 200);
+
+    }
+}
