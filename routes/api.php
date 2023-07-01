fedadc1358e006b7c7c45c2f6ab79eb5f8931f8c
ViniciusRCampos <vinicius_uo@hotmail.com>
2023-07-01 19:18:14 -0300

    refactor: Changing endpoint to handle offer requests

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/offers', [OffersController::class, 'getOffers']);
+Route::post('/', [OffersController::class, 'getOffers']);
